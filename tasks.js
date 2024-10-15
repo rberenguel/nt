941402ddd87346fdb29257e80f43a34e37b846c6
Ruben Berenguel Montoro <ruben@mostlymaths.net>
2024-10-15 20:29:29 +0200

    Leaked pending tasks hah

--- tasks.js
+++ tasks.js
@@ -9,60 +9,15 @@ const tasks = [
     },
   },
   {
-    projects: ["FooWrite", "Weave", "Lego", "drawing", "Drawing", "Post"],
+    projects: ["A", "B"],
   },
   {
-    text: "FooWrite for Unicorn",
+    text: "A for a",
   },
   {
-    text: "Pico DNS Server",
+    text: "B",
   },
   {
-    text: "Tree drawing course",
-  },
-  {
-    text: "Post about LLMs and project effort",
-    prio: 10,
-    color: "red",
-  },
-  {
-    text: "FooWrite: fix multiline loading refresh bug",
-    extra: "Multiline fails on load and up/down",
-  },
-  {
-    text: "FooWrite: debounce shift",
-    extra: "shift-key bounces",
-  },
-  {
-    text: "Weave: persistent file access",
-    extra: "",
-    color: "red",
-    prio: 6,
-  },
-  {
-    text: "Finish posting micro-pictures",
-    prio: 8,
-  },
-  {
-    text: "Local screenshots extension",
-    prio: 2,
-  },
-  {
-    text: "Lego 11 Sept",
-  },
-  {
-    text: "Lego Sau",
-  },
-  {
-    text: "Engraving drawing course",
-  },
-  {
-    text: "Post mentioning recent projects",
-  },
-  {
-    text: "Reduce Stardust scope",
-  },
-  {
-    text: "Backgammon bootcamp",
-  },
+    text: "C"
+  }
 ];
