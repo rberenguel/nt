9aaf630c1a458e6b86f41884130f22fc4a7b293f
Ruben Berenguel Montoro <ruben@mostlymaths.net>
2024-12-08 18:06:24 +0100

    Minor tweak of styling and ran prettier on everything

--- backgrounds.js
+++ backgrounds.js
@@ -3,6 +3,10 @@ const backgrounds = [
   "synthwave.jpg",
   "pencils.jpg",
   "bubbles.jpg",
+  "bubbles.jpg", // I particularly like this one
   "flows-78259.jpg",
   "creation.jpg",
+  "starry2024.jpg", // Extra seasonal weight
+  "starry2024.jpg",
+  "starry2024.jpg",
 ];
